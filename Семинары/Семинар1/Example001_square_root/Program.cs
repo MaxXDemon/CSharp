9c547e39446e5c168c996cb6ad1877c3d19a8932
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 16:14:18 +0700

    Example001_Square_root

---
+++ Семинары/Семинар1/Example001_square_root/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse (Console.ReadLine());
+int result = (int)Math.Pow (number, 2);
+Console.WriteLine ($"Квадратный корень {number}: {result}");
