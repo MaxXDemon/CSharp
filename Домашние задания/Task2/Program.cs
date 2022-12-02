4fa58ec5c3a40efd415ab1a9f1f1f6bf48843a6a
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 21:38:59 +0700

    Task2

---
+++ Домашние задания/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (number1!=number2 && number1>number2)
+{
+    max = number1;
+}
+else 
+    {
+        max = number2;
+    }
+    if (max!=number3 && max>number3)
+    {
+        Console.WriteLine($"Максимальное число {max}");
+    }
+    else 
+    {
+        Console.WriteLine($"Максимальное число {number3}");
+    }
