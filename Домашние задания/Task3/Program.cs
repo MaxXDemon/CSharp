e73bbdca046962efd8d218444b48fc203992b1aa
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 21:49:40 +0700

    Task3

---
+++ Домашние задания/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if (number%2 > 0 && number!=0)
+{
+    Console.WriteLine("Это число не является четным");
+}
+else    
+{
+    Console.WriteLine("Это число является четным");
+}
+    
