6b6885a3c88c0420e04ae6fe5112a16e69ad334a
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 16:50:12 +0700

    Example002_IfElse

---
+++ Семинары/Семинар1/Example002IfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+if (number1/number2 == number2)
+{
+    Console.WriteLine("Да");
+} else
+Console.WriteLine("Нет");
