e8ab9724f6a99cb52242cb0f18b3f24814b2e496
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 20:44:15 +0700

    Example003_2

---
+++ Семинары/Семинар1/Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Первая задача
+// Console.WriteLine("Введите число:");
+// int a = int.Parse(Console.ReadLine());
+// int b = (a-a*2);
+// Console.Write(b);
+// while (b<a)
+// {
+//     b=b+1;
+//     Console.Write($"{b} ");
+// }
+
+int number = int.Parse(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine (number%10);
+}
