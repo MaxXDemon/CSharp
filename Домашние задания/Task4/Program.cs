5d93836687f577f87e54842cf8ca0bb2da442f25
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 22:16:46 +0700

    Task4

---
+++ Домашние задания/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+while (i<number)
+{
+    if (i%2==0) 
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
