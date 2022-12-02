d2f9a4cf47c0ebe60c9b1ea05f4f09619b7e05ab
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 21:12:18 +0700

    Task1

---
+++ Домашние задания/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+if (number1==number2)
+{
+    Console.WriteLine("Числа равны");
+}
+    else
+    if (number1>number2)
+    {
+        Console.WriteLine($"{number1} больше {number2}");
+    }
+    else 
+    {
+        Console.WriteLine($"{number2} больше {number1}");
+    }
