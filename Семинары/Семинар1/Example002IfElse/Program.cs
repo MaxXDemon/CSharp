de57912fb05a3614a739d5de1ecf2fd4d19012a3
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-02 17:51:19 +0700

    Example002_IfElseSwitch

--- Семинары/Семинар1/Example002IfElse/Program.cs
+++ Семинары/Семинар1/Example002IfElse/Program.cs
@@ -1,9 +1,43 @@
-﻿Console.WriteLine("Введите первое число:");
-int number1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
-int number2 = int.Parse(Console.ReadLine());
-if (number1/number2 == number2)
+﻿// Это первая задача во втором задании
+// Console.WriteLine("Введите первое число:");
+// int number1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int number2 = int.Parse(Console.ReadLine());
+// if (number1/number2 == number2)
+// {
+//     Console.WriteLine("Да");
+// } else
+// Console.WriteLine("Нет");
+
+
+//Это вторая задача
+//Console.WriteLine("Введите число от 1 до 7:");
+int day = int.Parse(Console.ReadLine());
+
+switch(day)
 {
-    Console.WriteLine("Да");
-} else
-Console.WriteLine("Нет");
+    case 1: 
+    Console.WriteLine("Понедельник");
+        break;
+    case 2: 
+    Console.WriteLine("Вторник");
+        break;
+    case 3: 
+    Console.WriteLine("Среда");
+        break;
+    case 4: 
+    Console.WriteLine("Четверг");
+        break;
+    case 5: 
+    Console.WriteLine("Пятница");
+        break;
+    case 6: 
+    Console.WriteLine("Суббота");
+        break;
+    case 7: 
+    Console.WriteLine("Воскресенье");
+        break;
+    default: 
+    Console.WriteLine("Некорректное число");
+    break;
+}
