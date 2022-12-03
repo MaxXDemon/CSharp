caa5bff9ef070a2db15b4b51ea4b98ab77879b8b
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-03 22:28:37 +0700

    Example001

---
+++ Лекции/Лекция2/Example001/Program.cs
@@ -0,0 +1,38 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+int a1 = 51;
+int a2 = 65;
+int a3 = 31;
+
+int b1 = 41;
+int b2 = 51;
+int b3 = 16;
+
+int c1 = 71;
+int c2 = 18;
+int c3 = 99;
+
+int max1 = Max(a1, a2, a3);
+int max2 = Max(b1, b2, b3);
+int max3 = Max(c1, c2, c3);
+
+int max = Max(max1, max2, max3);
+
+// if (a2>max) max=a2;
+// if (a3>max) max=a3;
+
+// if (b1>max) max=b1;
+// if (b2>max) max=b2;
+// if (b3>max) max=b3;
+
+// if (c1>max) max=c1;
+// if (c2>max) max=c2;
+// if (c3>max) max=c3;
+
+Console.WriteLine (max);
