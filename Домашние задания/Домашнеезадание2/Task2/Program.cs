c0b8cb9f5739a7722146dff4b070792a7a014e43
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-04 15:10:53 +0700

    Task2

---
+++ Домашние задания/Домашнеезадание2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(0,10000000);
+Console.WriteLine(number);
+
+if (number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while ((number/10)>=100)
+    {
+        number=number/10;
+    }
+    Console.WriteLine(number%10);
+    
+}
+
