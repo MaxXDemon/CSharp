7835a3a2b18e07b151e461f507c709b124a33a6a
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-04 14:05:09 +0700

    Task004

---
+++ Семинары/Семинар2/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Введите первое число:");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if ((firstNumber/secondNumber==secondNumber) || (secondNumber/firstNumber==firstNumber))
+{
+    Console.WriteLine("Одно число является квадратом другого");
+}    
+else 
+{
+    Console.WriteLine("Первое число не является квдратом второго, а второе не является квдратом первого");
+}
+ 
+
+
