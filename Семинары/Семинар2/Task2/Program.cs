c0eedbb2b05dc34a6f51ab4e25b3035790c38698
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-04 12:38:34 +0700

    Task002

---
+++ Семинары/Семинар2/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+int firstNumber = rnd.Next(1,100);
+Console.WriteLine(firstNumber);
+
+int secondNumber = rnd.Next(1,100);
+Console.WriteLine(secondNumber);
+
+int resalt = secondNumber%firstNumber;
+if (resalt==0)
+{
+    Console.WriteLine("Второе число кратное первому");
+}
+else
+{
+    Console.WriteLine($"Второе число не кратное первому. Остаток от деления={resalt}");
+}
