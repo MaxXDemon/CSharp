ca5bd5287daf94c0ee090e88805217691febf2f5
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-04 13:26:28 +0700

    Task003

---
+++ Семинары/Семинар2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if ((number%7 == 0) && (number%23 == 0)) 
+{
+    Console.WriteLine($"Число {number} делится без остатка и на 7 и на 23");
+}
+else 
+{
+    Console.WriteLine($"Число {number} НЕ делится без остатка и на 7 и на 23");
+}
