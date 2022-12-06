93415a3e9d8a60b8a5de0319afb19c712cf4e0b4
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-07 01:44:13 +0700

    Task1

---
+++ Домашние задания/Домашнеезадание3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+char [] array = number.ToCharArray();
+Array.Reverse(array);
+string number2= new String(array);
+if (number==number2)
+{
+    Console.WriteLine("Это число является полиндромом");    
+}
+else
+{
+    Console.WriteLine("Это число НЕ является полиндромом");
+}
+
+// int length = array.Length(array);
+// Console.WriteLine(array[2]);
+// int [] array = new int [5];
+// double result = 1;
+// int index = 0;
+// int count = 10000;
+// if ((number%count!=0) && (number/count!=0))
+// while (count>0)
+// {
+//     array[index] = number/count;
+//     number=number-(number*count);
+//     count=count/10;
+//     index++;
+// }
+
+// Console.WriteLine(array[2]);
