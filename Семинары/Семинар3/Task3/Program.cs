1abff45236375528dab72762963388c546f2944e
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-06 21:44:49 +0700

    Task3

---
+++ Семинары/Семинар3/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+
+int n = int.Parse(Console.ReadLine());
+int square =-1;
+
+for (int index = 1; index<n; index++)
+{
+    //square=Math.Pow(index,2);
+    Console.Write($"{Math.Pow(index,2)}  ");
+} 
