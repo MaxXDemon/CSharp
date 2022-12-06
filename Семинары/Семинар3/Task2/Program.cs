422d8fd2f0a9544fc5dba22e05fa78c89d49bac6
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-06 21:07:46 +0700

    Task2

---
+++ Семинары/Семинар3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты первой точки по оси 'x':");
+double x1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки по оси 'y':");
+double y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки по оси 'x':");
+double x2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки по оси 'y':");
+double y2 = double.Parse(Console.ReadLine());
+
+double distance= Math.Sqrt(Math.Pow((x2-x1),2)+(Math.Pow((y2-y1),2)));
+
+Console.WriteLine(distance);
