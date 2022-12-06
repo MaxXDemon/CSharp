e9934d881b9e382d9d743fb14d50b66e0dd0bcee
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-06 16:27:28 +0700

    Task1

---
+++ Семинары/Семинар3/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int number = int.Parse(Console.ReadLine());
+
+
+switch (number)
+    {
+    case 1:
+        Console.WriteLine($"Координаты 'x' в диапазоне от 0 до бесконечности и 'y' в диапазоне от 0 до бесконечности");
+        break;
+    case 2:
+        Console.WriteLine("Координаты 'x' в диапазоне от минус бесконечности до 0 и 'y' в диапазоне от 0 до бесконечности");
+        break;
+    case 3:
+        Console.WriteLine("Координаты 'x' в диапазоне от минус бесконечности до 0 и 'y' в диапазоне от минус бесконечности до 0");
+        break;
+    case 4:
+        Console.WriteLine("Координаты 'x' в диапазоне от 0 до бесконечности и 'y' в диапазоне от минус бесконечности до 0");
+        break;
+    default:
+        break;
+    }
