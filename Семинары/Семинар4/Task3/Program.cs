a9b589d36bb0ebdc955e9caa5482de605599c47b
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-07 23:06:51 +0700

    Task3

---
+++ Семинары/Семинар4/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int [] array = new int [8];
+
+void FillArray(int[]array)
+{
+    int length = array.Length;
+    for (int index = 0; index < length; index++)
+    {
+        array[index]=new Random().Next(0,2);
+        Console.Write(array[index]);
+    }
+    return;
+}
+FillArray(array);
+
