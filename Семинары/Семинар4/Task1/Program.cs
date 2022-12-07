821731973aab51569fea39cfbb8c57239de7f13f
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-07 21:19:02 +0700

    Task1

---
+++ Семинары/Семинар4/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число: ");
+string number = Console.ReadLine();
+
+
+//int NumberOfDigits(string number)
+//{
+    char [] array = number.ToCharArray();
+    int quantity = array.Length;
+    //return (quantity);
+//}
+Console.WriteLine($"{quantity}");
