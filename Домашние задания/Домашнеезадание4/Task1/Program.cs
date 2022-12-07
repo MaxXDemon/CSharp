a8cf04152fe5753204f32fa5a87cf463fa9a8b8a
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-08 01:30:56 +0700

    Task1

---
+++ Домашние задания/Домашнеезадание4/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число, которое хотели бы возвести в степень: ");
+string input1 = Console.ReadLine();
+Console.WriteLine("Введите степень, в которую хотели бы возвести число: ");
+string input2 = Console.ReadLine();
+
+if ((int.TryParse(input1, out int number))&&(int.TryParse(input2, out int degree)))
+    {
+       
+              Console.WriteLine(Exponentiation(number, degree)); 
+    }
+else throw new ArgumentException("Вы ввели не число");
+
+int Exponentiation(int number, int degree)
+{
+    int result = 1;
+    for (int index = 0; index<degree; index++)
+    {
+        result=result*number;
+    }
+    return result;
+}
