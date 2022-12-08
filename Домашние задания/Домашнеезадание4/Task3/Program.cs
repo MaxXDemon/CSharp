d406938b43aad1f90ac32181c412779a0a3c922d
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-08 14:11:59 +0700

    Task3

---
+++ Домашние задания/Домашнеезадание4/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите размер массива:");
+int number = 0;
+PrintArray(ChekingNumber(Console.ReadLine()));
+
+int ChekingNumber(string Number)
+{
+    if (int.TryParse(Number, out int number))
+    {
+        return number;
+    }
+    else throw new ArgumentException("Вы ввели не число");
+}
+
+void PrintArray (int number)
+{
+    int[]array = new int [number];
+    for (int index = 0; index<number; index++)
+    {
+        array[index]= new Random().Next(0,10);
+        Console.Write(" "+array[index]);
+    }
+    
+}
