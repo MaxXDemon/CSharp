443df974044c921ee13be95b71e210e301768096
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-13 15:55:58 +0700

    Task4

---
+++ Семинары/Семинар6/Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿int ChekingNumber()
+{
+    if (int.TryParse(Console.ReadLine(), out int number))
+    {
+        return number;
+    }
+    else throw new ArgumentException("Вы ввели не число");
+}
+
+int[,] CreateAndPrintArray(int numberColumns, int numberRows)
+{
+    int[,] array = new int[numberColumns, numberRows];
+    for (int i = 0; i < numberColumns; i++)
+    {
+        for (int j = 0; j < numberRows; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void EvenIndexesSquared(int[,] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                summ += array[i,j];
+            }
+            
+        }
+        
+    }
+    Console.Write($"Сумма элекментов равна {summ}");
+}
+
+    Console.WriteLine("Введите размер двумерного массива");
+    EvenIndexesSquared(CreateAndPrintArray(ChekingNumber(), ChekingNumber()));
