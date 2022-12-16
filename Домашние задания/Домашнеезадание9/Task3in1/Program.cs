172d9baa07d1a6b050b63f40a49cfa0407455749
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-16 17:07:31 +0700

    first task of three

---
+++ Домашние задания/Домашнеезадание9/Task3in1/Program.cs
@@ -0,0 +1,31 @@
+﻿int CheckNumber()
+{
+    if (int.TryParse(Console.ReadLine(), out int number))
+        return number;
+    else throw new ArgumentException("Это не число, попробуйте снова");
+}
+
+void PrintNumbers(int firstNumber)
+{
+    if (firstNumber >= 1)
+    {
+        Console.Write($"{firstNumber} ");
+        PrintNumbers(firstNumber-1);
+    }
+    
+}
+
+Console.WriteLine("Введите номер задания от 1 до 3:");
+switch(CheckNumber())
+{
+    case (1): 
+        Console.WriteLine(@"Задача 1: Задайте значение N. 
+        Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+        Выполнить с помощью рекурсии.");
+        Console.WriteLine("Введите любое число: ");
+        int firstNumber = CheckNumber();
+        PrintNumbers(firstNumber);
+    break;
+
+}
+
